96244d689acfa89db03f15821e8a51f54bbdc61b
gildemberg-santos <gildemberg.santos@gmail.com>
2023-07-10 17:13:11 -0300

    Rubocop

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
@@ -5,40 +7,44 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get users_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_user_url
     assert_response :success
   end
 
-  test "should create user" do
+  test 'should create user' do
     assert_difference('User.count') do
-      post users_url, params: { user: { drescricao: @user.drescricao, email: @user.email, nome: @user.nome, password: @user.password } }
+      post users_url,
+           params: { user: { drescricao: @user.drescricao, email: @user.email, nome: @user.nome,
+                             password: @user.password } }
     end
 
     assert_redirected_to user_url(User.last)
   end
 
-  test "should show user" do
+  test 'should show user' do
     get user_url(@user)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_user_url(@user)
     assert_response :success
   end
 
-  test "should update user" do
-    patch user_url(@user), params: { user: { drescricao: @user.drescricao, email: @user.email, nome: @user.nome, password: @user.password } }
+  test 'should update user' do
+    patch user_url(@user),
+          params: { user: { drescricao: @user.drescricao, email: @user.email, nome: @user.nome,
+                            password: @user.password } }
     assert_redirected_to user_url(@user)
   end
 
-  test "should destroy user" do
+  test 'should destroy user' do
     assert_difference('User.count', -1) do
       delete user_url(@user)
     end
